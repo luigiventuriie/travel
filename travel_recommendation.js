2c3a4efd62d4f3349cce47a9eeef7b7fcb53abb1
Luigi Venturi <luigi.venturi.ie@gmail.com>
2024-11-13 12:14:43 -0500

    progress

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -17,4 +17,30 @@ function loadContent(page) {
         });
 }
 
+function fetchReccomendations(form){
+    const search = form.getElementsByTagName('input')[0].value;
+
+    fetch('travel_recommendation_api.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(json => {
+        const validKey = Object.keys(json).find( x=>  x.includes(search))
+        if(validKey){
+            return json[validKey]
+        }
+        else{
+            return []
+        }
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+
 loadContent("home.html")
+
